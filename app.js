9f942be300376854d1356b3ee9c3aae52cd1e1ce
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-06-25 02:06:02 +0530

    SecondChanges

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Hello World")
+console.dir(document);
+
+//console.log(document.domain);
+
+console.log(document.url);
+console.log(document.title)
+document.title = "Item2";
+
+console.log(document.doctype);
+console.log(document.head);
+
+var tittle = document.getElementById('header-title"');
+console.log(tittle);
+console.log(tittle.innerText);
+console.log(tittle.textContent);
