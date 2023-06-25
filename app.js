4e86b9dcf34d4e948ee24beea760cde6b32176d9
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-06-25 14:41:49 +0530

    Fourth commit

--- app.js
+++ app.js
@@ -17,12 +17,23 @@ console.log(document.getElementById('main-header'));
 //console.log(headerTittle.textContent);
 
 
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = "Hello";
+// items[1].style.backgroundColor = "yellow";
+
+//  for(let i=0;i<items.length;i++){
+//     items[i].style.backgroundColor = "blue";
+//  }
+
+var items = document.getElementsByTagName('li');
 console.log(items);
 console.log(items[1]);
 items[1].textContent = "Hello";
 items[1].style.backgroundColor = "yellow";
 
  for(let i=0;i<items.length;i++){
-    items[i].style.backgroundColor = "blue";
+    items[i].style.backgroundColor = "green";
  }
+
