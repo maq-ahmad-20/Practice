e2ec128989d2008e45f60ec98a8e26d8d76494d6
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-06-25 19:22:20 +0530

    EightCommit

---
+++ edit.js
@@ -0,0 +1,49 @@
+var form = document.getElementById('addForm');
+var items = document.getElementById('items');
+
+document.addEventListener('submit' , addItem);
+
+function addItem(e){
+    e.preventDefault();
+   
+     var addedText = document.getElementById('item').value;
+
+     var li = document.createElement('li');
+     li.className ='list-group-item';
+     li.appendChild(document.createTextNode(addedText));
+
+     var deleteButton = document.createElement('button');
+     deleteButton.className = 'btn btn-danger btn-sm float-right delete';
+     deleteButton.appendChild(document.createTextNode('X'));
+
+     var editButton = document.createElement('button');
+     editButton.className = 'btn btn-danger btn-sm float-right delete';
+     
+     editButton.appendChild(document.createTextNode("Edit"));
+     editButton.style.backgroundColor = 'green';
+
+     li.appendChild(editButton);
+     li.appendChild(deleteButton);
+     
+     
+     items.appendChild(li)
+
+
+}
+
+ 
+  document.addEventListener('click', (e)=>{
+
+     e.preventDefault();
+      if(e.target.classList.contains('delete')){
+            if(confirm("Are you sure to delete")){
+                var listtag = e.target.parentElement;
+                items.removeChild(listtag);
+            }
+
+      }
+  });
+
+
+
+ 
